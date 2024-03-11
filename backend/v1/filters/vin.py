06832172c62ebc911aa5baad7bcb40d130d1edde
Maurana <45948023+maurana@users.noreply.github.com>
2024-03-12 06:46:43 +0700

    Update vin.py

--- backend/v1/filters/vin.py
+++ backend/v1/filters/vin.py
@@ -2,7 +2,7 @@ from django_filters import rest_framework as filters
 from v1.models.vin import Vin
 
 class VinFilter(filters.FilterSet):
-    vin_number = filters.NumberFilter()
+    vin_number = filters.CharFilter()
 
     class Meta:
         model = Vin
