0ec86a981ba210d4d03796dd49b97d7d123c105e
maurana <maurana.hasegawa@gmail.com>
2024-03-17 21:18:31 +0700

    update dockerize of backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,8 +123,8 @@ DATABASES = {
         'NAME': config('MYSQL_DATABASE'),
         'USER': config('MYSQL_USER'),
         'PASSWORD': config('MYSQL_PASSWORD'),
-        'HOST': config('DB_HOST'),  # Use 'db' as default from .env
-        'PORT': config('DB_PORT'),  # Use '3306' as default from .env
+        'HOST': config('DB_HOST', 'db'),  # Use 'db' as default from .env
+        'PORT': config('DB_PORT','3306'),  # Use '3306' as default from .env
     }
 }
 
